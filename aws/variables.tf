7c431d386d0f6648c2606eb920cbc8701dbb3481
crspradlin <mail@crspradlin.org>
2021-09-30 20:54:35 -0400

    Updated documentation refering to optional domain configuration and setup instructions

--- aws/variables.tf
+++ aws/variables.tf
@@ -1,3 +1,4 @@
+# Required Variables
 variable "str_app_name" {
     description = "Name of the Application"
     type = string
@@ -8,6 +9,7 @@ variable "str_aws_account_id" {
     type = string
 }
 
+# Optional Variables
 variable "str_domain" {
     description = "Optional Domain to hook into API Gateway, Provide an Empty String if you do not have a Domain"
     type = string
