b0b7e9118cce93797ae3e88d68a5b43fb44ba290
crspradlin <github@crspradlin.org>
2022-12-07 12:00:00 +0000

    Added region string as tf app tfvar

--- aws/main.tf
+++ aws/main.tf
@@ -4,6 +4,8 @@ module "app" {
   str_aws_account_id = var.str_aws_account_id
   str_app_name = var.str_app_name
 
+  str_region = var.str_region
+
   int_dynamo_write_capacity = var.int_dynamo_write_capacity
   int_dynamo_read_capacity = var.int_dynamo_read_capacity
 
--- modules/variables.tf
+++ modules/variables.tf
@@ -1,6 +1,8 @@
 variable "str_aws_account_id" {}
 variable "str_app_name" {}
 
+variable "str_region" {}
+
 variable "int_dynamo_write_capacity" {}
 variable "int_dynamo_read_capacity" {}
 
