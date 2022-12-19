68e4869f3fb5a404eabdb897b6a7919c3a09b285
crspradlin <github@crspradlin.org>
2022-12-19 12:00:00 +0000

    Added tests for gunzip

--- modules/services/code/sesUtils.test.js
+++ modules/services/code/sesUtils.test.js
@@ -1,4 +1,5 @@
 const AWS = require('aws-sdk');
+const zlib = require('zlib');
 const sesUtils = require('./sesUtils');
 
 const errorRepository = require('./errorRepository');
@@ -20,10 +21,38 @@ jest.mock('aws-sdk', () => {
     }
 });
 
+let mockCallbackErrorParam = null;
+let base64DataParam = 'base64Data';
+jest.mock('zlib', () => {
+    return {
+        gunzip: (bufferedData, callback) => {
+            callback(mockCallbackErrorParam, JSON.stringify({mockResponse: mockResponse}));
+            expect(bufferedData).equal(base64DataParam);
+        }
+    }
+});
+
 describe('Test sesUtils', () => {
     beforeEach(() => {
         AWS // Needed for eslint usage
+        zlib
     });
+
+    test('Test gunzip call', async () => {
+        const response = await sesUtils.gunzip(base64DataParam);
+        expect(response).toEqual('{\n  "mockResponse": "mockResponse"\n}');
+    });
+
+    test('Test gunzip call with error', async () => {
+        const expectedError = new Error('test error');
+        mockCallbackErrorParam = expectedError;
+        try {
+            await sesUtils.gunzip(base64DataParam);
+        } catch (error) {
+            expect(error).toEqual(expectedError);
+        }
+        expect.assertions(1);
+    })
 
     test('Test sendToSupportEmail call', async () => {
         process.env.APP_SUPPORT_EMAIL = 'example@example.com';
