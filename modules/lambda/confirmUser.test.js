62ef8edce9fbea57de5d9658482151220f9a99cd
crspradlin <github@crspradlin.org>
2024-03-05 20:51:22 -0500

    Updated tests for confirmUser lambda

--- modules/lambda/confirmUser.test.js
+++ modules/lambda/confirmUser.test.js
@@ -5,6 +5,7 @@ const confirmUser = require('./confirmUser');
 const mockCognitoService = { };
 const mockUserUtils = { };
 const mockDynamoService = { };
+const mockStatesUtils = { };
 const mockCreateAPIResponse = { };
 
 let instance;
@@ -16,6 +17,7 @@ describe('Test confirmUser', () => {
             cognitoService: mockCognitoService,
             userUtils: mockUserUtils,
             dynamoService: mockDynamoService,
+            statesUtils: mockStatesUtils,
             createAPIResponse: mockCreateAPIResponse,
             event: event
         }
@@ -23,7 +25,7 @@ describe('Test confirmUser', () => {
         instance = confirmUser.confirmUserService(deps);
     })
 
-    test('Test handler call', async () => {
+    test('Test handler call for API Gateway request', async () => {
         instance.event = {
             body: JSON.stringify({
                 profile: 'mockProfile',
@@ -31,8 +33,9 @@ describe('Test confirmUser', () => {
             })
         };
         mockCognitoService.confirmUser = jest.fn().mockResolvedValue('mockCognitoResponse');
-        mockUserUtils.getUser = jest.fn().mockResolvedValue({name: 'mockUserName'});
+        mockUserUtils.getUser = jest.fn().mockResolvedValue({name: 'mockUserName', confirmationToken: 'mockConfirmationToken'});
         mockDynamoService.update = jest.fn().mockResolvedValue(true);
+        mockStatesUtils.sendTaskSuccess = jest.fn().mockResolvedValue(true);
         mockCreateAPIResponse.Ok = jest.fn().mockReturnValue('mockAPIResponse');
 
         const response = await instance.handler();
@@ -47,6 +50,84 @@ describe('Test confirmUser', () => {
                 ':value': true
             }
         });
+        expect(mockStatesUtils.sendTaskSuccess).toHaveBeenCalledWith('mockConfirmationToken');
+    });
+
+    test('Test handler call for Step Function request without task token', async () => {
+        instance.event = {
+            userProfile: 'mockUserProfile'
+        };
+        const mockRemoveUserResponse = {name: 'mockUserName', confirmationToken: 'mockConfirmationToken'};
+        mockUserUtils.removeUser = jest.fn().mockResolvedValue(mockRemoveUserResponse);
+
+        const response = await instance.handler();
+
+        expect(response).toEqual(mockRemoveUserResponse);
+        expect(mockUserUtils.removeUser).toHaveBeenCalledWith('mockUserProfile');
+    });
+
+    test('Test handler call for Step Function request with task token', async () => {
+        instance.event = {
+            userProfile: 'mockUserProfile',
+            taskToken: 'mockTaskToken'
+        };
+        const expectedAdditionalConfig = {
+            ExpressionAttributeNames: {
+                '#key': 'confirmationToken'
+            },
+            ExpressionAttributeValues: {
+                ':value': 'mockTaskToken'
+            }
+        };
+
+        const mockDynamoServiceResponse = 'mockedResponse';
+        mockDynamoService.update = jest.fn().mockResolvedValue(mockDynamoServiceResponse);
+
+        const response = await instance.handler();
+
+        expect(response).toEqual(mockDynamoServiceResponse);
+        expect(mockDynamoService.update).toHaveBeenCalledWith('UserData', {profile: 'mockUserProfile'}, 'set #key = :value', expectedAdditionalConfig);
+    });
+
+    test('Test handler call for Step Function request with known exception thrown', async () => {
+        instance.event = {
+            userProfile: 'mockUserProfile',
+            taskToken: 'mockTaskToken'
+        };
+
+        const mockError = errorRepository.createError(1403, new Error('repo error'));
+        mockDynamoService.update = jest.fn().mockImplementation(() => {
+            throw mockError;
+        });
+
+        try {
+            await instance.handler();
+        } catch(error) {
+            expect(error).toEqual(mockError);
+        }
+
+        expect.assertions(1);
+    });
+
+    test('Test handler call for Step Function request with unknown exception thrown', async () => {
+        instance.event = {
+            userProfile: 'mockUserProfile',
+            taskToken: 'mockTaskToken'
+        };
+
+        const mockError = new Error('repo error');
+        mockDynamoService.update = jest.fn().mockImplementation(() => {
+            throw mockError;
+        });
+
+        const expectedError = errorRepository.createError(1000, mockError);
+        try {
+            await instance.handler();
+        } catch(error) {
+            expect(error).toEqual(expectedError);
+        }
+
+        expect.assertions(1);
     });
 
     test('Test handler call with unknown caught error', async () => {
@@ -118,6 +199,7 @@ describe('Test confirmUser', () => {
         jest.mock('/opt/cognitoService', () => { return { } }, {virtual: true});
         jest.mock('/opt/userUtils', () => { return {default: () => {return { }}}}, {virtual: true});
         jest.mock('/opt/dynamoService', () => { return { } }, {virtual: true});
+        jest.mock('/opt/statesUtils', () => { return { } }, {virtual: true});
         jest.mock('/opt/createAPIResponse', () => { return { } }, {virtual: true});
         const mockEvent = 'mockEvent';
 
@@ -135,6 +217,7 @@ describe('Test confirmUser', () => {
             cognitoService: { },
             userUtils: { },
             dynamoService: { },
+            statesUtils: { },
             createAPIResponse: { },
             event: mockEvent
         })
